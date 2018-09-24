eb5f84beedf6a305b4229e12f07861a637901621
Carrot Pop <petrsvar@users.noreply.github.com>
2018-09-24 23:50:52 +0200

    Add files via upload

--- Corona/misc/appRoutines.lua
+++ Corona/misc/appRoutines.lua
@@ -106,6 +106,28 @@ M.localToUTC = function(y, m, d, h, s)
 	-- return UTC time as string
 	return utcTime
 end
+
+M.localToWeekday = function(y, m, d, h, s)
+
+    -- takes local year, month, day, hour and minutes as numbers or strings, 
+    -- calculates UTC time and outputs in 2018-01-18T07:30:00.000Z format
+
+	local utcTime = ""
+
+	-- translate to seconds
+	local time1 = os.time {
+		year = tonumber(y), 
+		month = tonumber(m), 
+		day = tonumber(d), 
+		hour = tonumber(h), 
+		sec = tonumber(s)
+	}
+
+	local weekDay = os.date("%a", time1)
+
+	-- return UTC time as string
+	return weekDay
+end
 -- -------------------------------------------------------------------------
 
 M.UTCtoLocal = function(utcTime)
@@ -165,6 +187,7 @@ M.minutesToHours = function(time_offset)
         -- return hours:minutes
         return time
 end
+
 
 -- ------------------------------------------------------------------------- --
 -- MAPS
